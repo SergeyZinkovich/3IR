2c325be0b5fb5fe9475c317077cbc0e8b1c50690
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-07-10 20:12:11 +1000

    Rewrite UI in OOP

--- js/UI.js
+++ js/UI.js
@@ -1,3 +1,4 @@
+"use strict"
 $(document).ready(function() {
     console.log('UI loaded')
 
@@ -5,33 +6,37 @@ $(document).ready(function() {
     const GAME_GRID_HEIGHT = 8;
     const CELL_SIZE = 100;
 
-    //create game grid
-    var gameGrid = $('#game-grid');
-    for (let i = 0; i < GAME_GRID_HEIGHT; ++i) {
-        for (let j = 0; j < GAME_GRID_WIDTH; ++j) {
-            var id = i + '-' + j;
-            if (j === 3) {
-                gameGrid.append('<div id="' + id +'" class="game-cell"><img src="img/diamond-blue.png"></div>');
-            }
-            else {
-                gameGrid.append('<div id="' + id +'" class="game-cell"><img src="img/diamond-red.png"></div>');
-            }
-            // $('#' + id).css('background-color', getRandomColor());
-            $('#' + id).click({gameGrid: gameGrid}, userClick);
-        }
-    }
+    var Game = function () { 
+        //create game grid
+        this.gameGrid = $('#game-grid');
 
-
-    function userClick(e) {
-        $(this).addClass('selected-cell');
-        var selectedCells = e.data.gameGrid.children(".selected-cell");
-        if (selectedCells.length === 2) {
-            swapCells(selectedCells.eq(0), selectedCells.eq(1));
-            gameGrid.children().removeClass('selected-cell');
+        for (let i = 0; i < GAME_GRID_HEIGHT; ++i) {
+            for (let j = 0; j < GAME_GRID_WIDTH; ++j) {
+                var id = i + '-' + j;
+                if (j === 3) {
+                    this.gameGrid.append('<div id="' + id +'" class="game-cell"><img src="img/diamond-blue.png"></div>');
+                }
+                else {
+                    this.gameGrid.append('<div id="' + id +'" class="game-cell"><img src="img/diamond-red.png"></div>');
+                }
+                // $('#' + id).css('background-color', getRandomColor());
+                $('#' + id).click({game: this}, function() {
+                    e.data.game.userClick($(this));
+                });
+            }
         }
     };
 
-    function swapCells(cell1, cell2) {
+    Game.prototype.userClick = function(cell){
+        cell.addClass('selected-cell');
+        var selectedCells = this.gameGrid.children(".selected-cell");
+        if (selectedCells.length === 2) {
+            this.swapCells(selectedCells.eq(0), selectedCells.eq(1));
+            this.gameGrid.children().removeClass('selected-cell');
+        }
+    };
+
+    Game.prototype.swapCells = function(cell1, cell2) {
         var id1 = cell1.attr('id').split('-');
         var id2 = cell2.attr('id').split('-');
 
@@ -57,14 +62,7 @@ $(document).ready(function() {
                 cell2Img.css({'top': 0, 'left': 0});
                 cell2Img.attr('src', cell1ImgPath);
         });
-    }
-
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (let i = 0; i < 6; ++i) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-      return color;
     };
+
+    var game = new Game();
 });
