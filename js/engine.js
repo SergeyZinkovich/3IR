62231d1c58c474bce6f06a2b1b77eb79d983d2c5
Sergei <my_agent-1@mail.ru>
2017-07-10 23:50:19 +1000

    Added gemesFall and check for possibility of anigilation

--- js/engine.js
+++ js/engine.js
@@ -7,12 +7,10 @@ var gems = [0, 1, 2, 3, 4];
 var myGen = new levelGenerator(gems, 8, 8);
 myGen.generateLevel();
 var playingField = myGen.getLevel();
-check();
+//check();
 
 function turn(fromX, fromY, toX, toY){
-	let buff = playingField[fromX][fromY];
-	playingField[fromX][fromY] = playingField[toX][toY];
-	playingField[toX][toY] = buff;
+	swap(playingField[fromX][fromY], playingField[toX][toY]);
 }
 
 function check(){
@@ -38,6 +36,7 @@ function anigilate(){
 						playingField[i][h] = -1;
 						ans.push([i, h]);
 					}
+					gemFall(ans);
 					doGen();
 					return ans;
 				}
@@ -58,6 +57,7 @@ function anigilate(){
 						playingField[h][i] = -1;
 						ans.push([h, i]);
 					}
+					gemFall(ans);
 					doGen();
 					return ans;
 				}
@@ -68,6 +68,57 @@ function anigilate(){
 		}
 	}
 	return false;
+}
+
+function canAnigilate(){
+	let beg;
+	for (let i = 0; i < playingField.length; i++){
+		beg = 0;
+		for (let j = 0; j < playingField[0].length; j++){
+			if ((playingField[i][j] != playingField[i][beg]) || (j === playingField[0].length - 1)){
+				if (playingField[i][j] === playingField[i][beg]){j++;}
+				if (j - beg > 2){
+					return true;
+				}
+				else{
+					beg = j;
+				}
+			}
+		}
+	}
+	for (let i = 0; i < playingField[0].length; i++){
+		beg = 0;
+		for (let j = 0; j < playingField.length; j++){
+			if ((playingField[j][i] != playingField[beg][i]) || (j === playingField.length - 1)){
+				if (playingField[j][i] === playingField[beg][i]){j++;}
+				if (j - beg > 2){
+					return true;
+				}
+				else{
+					beg = j;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function gemFall(line){
+	for (let i = line[0][0]; i <= line[-1][0]; i++){
+		for (let j = line[0][1]; j <= line[-1][1]; j++){
+		let h = j;
+			while ((h > 0) && (playingField[i][h - 1] !== -1)){
+				swap(playingField[i][h - 1], playingField[i][h]);
+			}
+		}
+	}
+}
+
+function swap(a, b){
+	let c;
+	c = a;
+	a = b;
+	b = c;
 }
 
 function doGen(){
